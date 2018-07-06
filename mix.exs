31925da72799a5d24f3f9f985d9208f674ada3fd
Connor Rigby <konnorrigby@gmail.com>
2018-07-05 21:15:22 -0700

    Add license and stuff

--- mix.exs
+++ mix.exs
@@ -4,12 +4,28 @@ defmodule NifGen.MixProject do
   def project do
     [
       app: :nif_gen,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
+
+  defp description,
+    do: """
+    Generate an Elixir project with a nif.
+    """
+
+  defp package,
+    do: [
+      licenses: ["MIT"],
+      maintainers: ["Connor Rigby"],
+      links: %{"GitHub" => "https://github.com/connorrigbyy/nif_gen"},
+      source_url: "https://github.com/connorrigbyy/nif_gen",
+      homepage_url: "https://github.com/connorrigbyy/nif_gen"
+    ]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -21,8 +37,7 @@ defmodule NifGen.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.18.3", only: :dev},
     ]
   end
 end
